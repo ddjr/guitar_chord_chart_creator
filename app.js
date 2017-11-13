f0a226c8bb275d052583e3fb21eaa20689409d17
David Daly <ddjr@Davids-MBP.fios-router.home>
2017-11-13 13:36:57 -0600

    added fretnumber to side. PDF now also shows fret number and chord names.

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
+var chordsPerLine = 6;
+var lines = 5;
+// var strings = 6;
+// var frets = 5;
+
 var chords = [];
-var numChords = 6;
+var numChords = chordsPerLine * lines;
 window.onload = function() {
   var FPS = 30;
-  for(i = 0; i<numChords; i++) {
+  for(let i=0; i<numChords; i++) {
     chords.push(new Chord());
   }
-  console.log(chords);
+  //console.log(chords);
   setInterval(function() {
     draw();
   }, 1000/FPS);
-
 }
-
 function draw() {
-  for(i = 0; i<5; i++) {
+  for(i = 0; i<numChords; i++) {
     chords[i].draw();
   }
 }
--- chord.js
+++ chord.js
@@ -1,45 +1,82 @@
 function Chord() {
-  this.canvas = document.createElement('canvas');
-  this.canvas.width = 150;
-  this.canvas.hieght = 120;
-  var canvas = this.canvas;
+  // ------------------------
+  // |    Variable Setup    |
+  // ------------------------
+  var canvas = document.createElement('canvas');
   var canvasContext = canvas.getContext('2d');
-  var body = document.getElementsByTagName("body")[0];
-  var width = this.canvas.width;
-  var hieght = this.canvas.hieght;
+  canvas.width = 150;
+  canvas.height = 200;
 
+  // offsets
+  var chord_name_offset = 0;
+  var open_string_offet = 15;
+  var fret_number_offset = 15;
+
+  var width = canvas.width - fret_number_offset;
+  var height = canvas.height - (chord_name_offset + open_string_offet);
   var frets = 5;
   var strings = 6;
-  var offset_top = 0;
-  var offset_left = 0;
 
+  var radius = height/(frets*3.5);
+
+  this.fingerings = [];
   var fingerings = [];
   for(let i=0; i<strings*frets; i++) {
-    fingerings.push(0);
+    this.fingerings.push(0);
   }
+  fingerings = this.fingerings;
+  this.openStrings = [];
   var openStrings = [];
   for(let i=0; i<strings; i++) {
-    openStrings.push(0);
+    this.openStrings.push(0);
   }
+  openStrings = this.openStrings;
 
-  // console.log(fingerings.length);
+  // HTML (DOM) Setup
+  var div1 = document.createElement('div');
+  var div2 = document.createElement('div');
+  this.container = document.createElement('div');
+  this.container.className += ' col-6 col-sm-3 col-md-2';
+  div2.style.width = 'fit-content';
+  this.chordName = document.createElement('input');
+  this.fretNumber = document.createElement('input');
+  this.chordName.placeholder = " ";
+  this.fretNumber.placeholder = "1";
+  this.container.appendChild(div1);
+  div1.appendChild(this.chordName);
+  div1.appendChild(this.fretNumber);
+  this.chordName.align = "center";
+  this.container.appendChild(div2);
+  div2.appendChild(canvas);
+  document.getElementsByTagName('div')[1].appendChild(this.container);
 
 
-  body.appendChild(canvas);
-  this.canvas.addEventListener('click', function(evt) {
+
+
+  // ------------------------
+  // |     Mouse Events     |
+  // ------------------------
+  canvas.addEventListener('click', function(evt) {
     var mousePos = calculateMousePos(evt);
 
-    if(mousePos.y > 0 && mousePos.y < 10){
-      console.log("here");
+    mouseX = mousePos.x - fret_number_offset;
+    mouseY = mousePos.y - chord_name_offset;
+
+    // handles clicking on open string/mute string symbols
+    if( mouseX > 0 && mouseX < width &&
+        mouseY > 0 && mouseY < open_string_offet) {
+        var index = Math.floor(mouseX / (width/strings));
+        openStrings[index] = (openStrings[index]+1) % 3;
     }
-    if(mousePos.x > 0 && mousePos.x < width &&
-       mousePos.y > 0 && mousePos.y < hieght) {
 
+    mouseY -= open_string_offet;
+    // handles clicking on frets
+    if(mouseX > 0 && mouseX < width &&
+       mouseY > 0 && mouseY < height) {
 
-      var string = Math.floor(mousePos.x / (width/strings));
-      var fret = Math.floor((mousePos.y) / (hieght/frets));
-      var index = (fret * strings) + string;
-      console.log(index);
+      var string = Math.floor(mouseX / (width/strings)) +1;
+      var fret = Math.floor(mouseY / (height/frets)) +1 ;
+      var index = ((fret-1) * strings) + (string -1);
       if(fingerings[index] == 0) {
         fingerings[index] = 1;
       } else {
@@ -48,48 +85,77 @@ function Chord() {
     }
   });
 
+  // ------------------------
+  // |      Draw Chord      |
+  // ------------------------
   this.draw = function() {
+    this.fingerings = fingerings;
+    this.openStrings = openStrings;
+    // clear background
+    drawRect(0, 0, canvas.width, canvas.height, "white");
+    canvasContext.save();
+    this.drawFretNumber();
+    canvasContext.translate(fret_number_offset, chord_name_offset);
+    this.drawOpenStringsSymbols();
+    canvasContext.translate(0, open_string_offet);
     this.drawFretboard();
-    this.drawingFingering();
+    this.drawFingering();
+    canvasContext.restore();
   }
-  this.drawingFingering = function() {
-
-    for(let i=0; i<fingerings.length; i++) {
-      if(fingerings[i] == 1) {
-        var currentString = (i % strings) + 1;
-        var currentFret = Math.floor(i/strings);
-
-
-        var string_offset = width/(strings*2);
-        fingerX = (currentString-1)*(width/strings) + string_offset;
-        fingerY = hieght*currentFret/frets + hieght*1/(frets*2);
-        radius = hieght/(frets*3.3);
-
-        drawCircle(fingerX,fingerY, radius, "black");
-
+  this.drawOpenStringsSymbols = function() {
+    canvasContext.font = "12px helvetica";
+    for(let i=0; i<openStrings.length; i++) {
+      var string_offset = width/(strings*2);
+      if(openStrings[i] == 1) {
+        colorText("O", (i)*(width/strings) + string_offset-4, open_string_offet-4, "black");
+      } else if (openStrings[i] == 2) {
+        colorText("X", (i)*(width/strings) + string_offset-4, open_string_offet-4, "black");
       }
     }
   }
-  this.drawFretboard = function() {
-    // clear background
-    drawRect(0, 0, width, hieght, "white");
+  this.drawFingering = function() {
+    var string_offset = width/(strings*2);
+    var distance_to_center_of_fret = height/(frets*2);
+    for(let i=0; i<this.fingerings.length; i++) {
+      var currentString = (i % strings) + 1;
+      var currentFret = Math.floor(i/strings);
+      var fingerX = (currentString-1) * (width/strings) + string_offset;
+      var fingerY = currentFret * (height/frets) + distance_to_center_of_fret;
 
+      if(this.fingerings[i] == 1) {
+        drawCircle(fingerX,fingerY, radius, "black");
+      }
+      // console.log();
+      // // debugConsoleLog();
+      // if(this.fingerings[i] == 1) {
+      //   drawCircle(fingerX,fingerY, radius, "black");
+      //   drawCircle(fingerX,fingerY, radius*.9, "white");
+      //   colorText("1", fingerX-3,fingerY+3, "black");
+      // }
+    }
+  }
+  this.drawFretboard = function() {
     var string_offset = width/(strings*2);
     // draw strings
     for(let i=0; i<strings; i++){
       var string_spacing = width/strings;
-      drawRect(i*string_spacing + string_offset, 0, 1, hieght, "black");
+      drawRect(i*string_spacing + string_offset, 0, 1, height, "black");
     }
     // draw frets
     for(let i=0; i<=frets; i++) {
       var fret_width = (strings-1)*(width/strings);
       if(i == 0) {
-        drawRect(string_offset, i*(hieght/frets), fret_width,1,  "black");
+        drawRect(string_offset, i*(height/frets), fret_width,1,  "black");
       }
-      drawRect(string_offset, i*(hieght/frets) - 1, fret_width ,1,  "black");
+      drawRect(string_offset, i*(height/frets) - 1, fret_width ,1,  "black");
     }
-
   }
+
+  this.drawFretNumber = function() {
+    canvasContext.font = "20px helvetica";
+    colorText(this.fretNumber.value,1,height/(frets) +3,"black");
+  }
+
 
   function drawCircle(centerX, centerY, radius, color) {
     canvasContext.fillStyle = color;
@@ -103,20 +169,21 @@ function Chord() {
   }
   function colorText(showWords, textX,textY, fillColor) {
     canvasContext.fillStyle = fillColor;
+    canvasContext.alignText = "center";
     canvasContext.fillText(showWords, textX,textY);
   }
 
   function calculateMousePos(evt) {
-  var rect = canvas.getBoundingClientRect();
-  var root = document.documentElement;
-  var mouseX = evt.clientX - rect.left - root.scrollLeft;
-  var mouseY = evt.clientY - rect.top - root.scrollTop;
-  return {
-    x: mouseX,
-    y: mouseY
-  };
-}
-
-
-
+    var rect = canvas.getBoundingClientRect();
+    var root = document.documentElement;
+    var mouseX = evt.clientX - rect.left - root.scrollLeft;
+    var mouseY = evt.clientY - rect.top - root.scrollTop;
+    return {
+      x: mouseX,
+      y: mouseY
+    };
+  }
+  function debugConsoleLog() {
+    console.log();
+  }
 }
